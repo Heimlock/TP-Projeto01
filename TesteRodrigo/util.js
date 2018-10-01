3fae390b5c706e6a4116de9bb0f2ad75e3f146bd
RODRIGO DA SILVA CARDOSO <rodrigo.sc5@puccampinas.edu.br>
2018-10-01 10:43:58 -0300

    Aula 8

---
+++ TesteRodrigo/util.js
@@ -0,0 +1,13 @@
+exports.dateTime = function(){  //cria um módulo
+    return Date();
+};
+
+exports.createUser = function(){
+    var x = 10;
+    var y = 22;
+    return x + y;
+};
+
+exports.test = function(){
+    return 'Teste!';
+}
